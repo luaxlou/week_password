bd546736f881d489363f6657eebada84e408b386
Jhon <john@ezjie.com>
2016-06-30 19:15:56 +0800

    为增加week password sample 增加top25

---
+++ scripts/merge_dict.rb
@@ -0,0 +1,23 @@
+dict1 = ARGV[0]
+dict2 = ARGV[1]
+dict3 = ARGV[2]
+
+Dir.chdir('./')
+
+
+dict1_arr = File.open(dict1).readlines.map do |l|
+	l.chomp
+end  
+
+File.open(dict2).each_line do |l|
+	   l.chomp!
+	  if(!dict1_arr.include?(l))
+
+	  	dict1_arr.push l
+
+	  end 
+
+	 
+end
+
+File.write dict3,dict1_arr.join("\n")
