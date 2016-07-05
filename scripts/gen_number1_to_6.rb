5b0fa0c0500b971ea473ec867aac18b8fd2dd039
Jhon <--g>
2016-07-06 00:22:47 +0800

    优化1 to 6

--- scripts/gen_number1_to_6.rb
+++ scripts/gen_number1_to_6.rb
@@ -1,7 +1,19 @@
     Dir.chdir('./tmp/pre')
     File.open("number_1_to_6.txt", "wb") do |file| 
 
-		(1.. 999999).each do |n|
+    	(0.. 99999).each do |n|
+
+
+     		l = n.to_s.length
+    			(1..l).each do |j| do
+	    			nw = '0'*(6-j) +n.to_s
+	 		 	  	file.write("#{nw}\n")
+ 		 	  end 
+ 		 	  
+		 	
+		end
+
+		(100000.. 999999).each do |n|
  		 	  	file.write("#{n}\n")
 		 	
 		end
